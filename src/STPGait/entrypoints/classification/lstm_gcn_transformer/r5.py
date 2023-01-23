f91804982a1a30927b3b2aabd432034e99c71942
AlirezaDizaji <alirezadizaji@yahoo.com>
2023-01-23 18:12:00 +0330

    New runs

---
+++ src/STPGait/entrypoints/classification/lstm_gcn_transformer/r5.py
@@ -0,0 +1,39 @@
+from typing import Tuple
+
+import torch
+from torch.utils.data import DataLoader
+
+from ....config import BaseConfig, TrainingConfig
+from ....context import Skeleton
+from ....data.read_gait_data import ProcessingGaitConfig
+from ....dataset.KFold import GraphSkeletonKFoldOperator, SkeletonKFoldConfig, KFoldConfig
+from ....enums import Optim, Label
+from ....models import GCNLSTMTransformerV2
+from ....preprocess.main import PreprocessingConfig
+from .r2 import Entrypoint as E
+from .r1 import Entrypoint as EE
+
+IN = Tuple[torch.Tensor, torch.Tensor, torch.Tensor]
+OUT = Tuple[torch.Tensor, torch.Tensor, torch.Tensor]
+
+# try 6 (try 3 ->)
+## Use only anxious-hypokinetic_frontal-healthly
+class Entrypoint(E):
+    def __init__(self) -> None:
+        kfold = GraphSkeletonKFoldOperator(
+            config=SkeletonKFoldConfig(
+                kfold_config=KFoldConfig(K=5, init_valK=0, init_testK=0, remove_labels=[Label.ATAXIC, Label.PARETIC, Label.SENSORY_ATAXIC]),
+                load_dir="../../Data/output_1.pkl",
+                filterout_unlabeled=True,
+                savename="processed_120c.pkl",
+                proc_conf=ProcessingGaitConfig(preprocessing_conf=PreprocessingConfig(critical_limit=120)))
+            )
+        config = BaseConfig(
+            try_num=6,
+            try_name="lstm_gcn_transformer",
+            device="cuda:0",
+            eval_batch_size=32,
+            save_log_in_file=True,
+            training_config=TrainingConfig(num_epochs=200, optim_type=Optim.ADAM, lr=3e-3, early_stop=50)
+        )
+        super(E, self).__init__(kfold, config)
---
+++ src/STPGait/entrypoints/classification/lstm_gcn_transformer/r7.py
@@ -0,0 +1,39 @@
+import os
+from typing import List, Tuple
+import pickle
+
+import numpy as np
+import torch
+
+from ....config import BaseConfig, TrainingConfig
+from ....data.read_gait_data import ProcessingGaitConfig
+from ....dataset.KFold import GraphSkeletonKFoldOperator, SkeletonKFoldConfig, KFoldConfig
+from ....enums import Optim, Separation, Label
+from ....models import GCNLSTMTransformer
+from ....preprocess.main import PreprocessingConfig
+from .r1 import Entrypoint as E
+
+IN = Tuple[torch.Tensor, torch.Tensor, torch.Tensor]
+OUT = Tuple[torch.Tensor, torch.Tensor, torch.Tensor]
+
+# try 8 (try 2 ->)
+## Use only ataxic-hypokinetic_frontal-healthly
+class Entrypoint(E):
+    def __init__(self) -> None:
+        kfold = GraphSkeletonKFoldOperator(
+            config=SkeletonKFoldConfig(
+                kfold_config=KFoldConfig(K=5, init_valK=0, init_testK=0, remove_labels=[Label.ANXIOUS, Label.PARETIC, Label.SENSORY_ATAXIC]),
+                load_dir="../../Data/output_1.pkl",
+                filterout_unlabeled=True,
+                savename="processed_120c.pkl",
+                proc_conf=ProcessingGaitConfig(preprocessing_conf=PreprocessingConfig(critical_limit=120)))
+            )
+        config = BaseConfig(
+            try_num=8,
+            try_name="lstm_gcn_transformer",
+            device="cuda:0",
+            eval_batch_size=1,
+            save_log_in_file=True,
+            training_config=TrainingConfig(num_epochs=200, optim_type=Optim.ADAM, lr=3e-3, early_stop=50)
+        )
+        super(E, self).__init__(kfold, config)
