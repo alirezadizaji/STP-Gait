Fix: edge weighting

src/STPGait/entrypoints/classification/gcn/try38.py
@@ -49,7 +49,7 @@ class Entrypoint(TrainEntrypoint[IN, OUT, BaseConfig]):
         self._edge_index_lower: torch.Tensor = None
 
     def get_model(self) -> nn.Module:
-        num_classes = np.unique(self.kfold.get_labels()).size - 1
+        num_classes = self.kfold._ulabels.size
         print(f"*** Train GCNSemiSupervised on {num_classes} classes. ***", flush=True)
         model = GCNSemiSupervised(dim_node=2, dim_hidden=60, sup_num_classes=num_classes, unsup_num_classes=num_classes)
         return model

src/STPGait/entrypoints/classification/gcn/try40.py
@@ -1,18 +1,18 @@
 import torch
-from torch import nn
-from torch_geometric.data import Data, Batch
+from torch_geometric.data import Batch, Data
 
 from ....config import BaseConfig, TrainingConfig
+from ....context import Skeleton
 from ....dataset.KFold import GraphSkeletonKFoldOperator, SkeletonKFoldConfig, KFoldConfig
 from ....data.read_gait_data import ProcessingGaitConfig
 from ....enums import Optim
-from ....models import GCN_3l_BN
+from ....models.gcn_semisupervised import calc_edge_weight
 from ....preprocess.main import PreprocessingConfig
-from .try17 import Entrypoint as E
+from .try18 import Entrypoint as E
 from ...train import TrainEntrypoint
 
-# try 27 (try 17 ->)
-## Approximate and use Z feature too.
+# try 40 (try 18 ->)
+## Applying non-temporal edges
 class Entrypoint(E):
     def __init__(self) -> None:
         kfold = GraphSkeletonKFoldOperator(
@@ -20,12 +20,12 @@ class Entrypoint(E):
                 kfold_config=KFoldConfig(K=5, init_valK=0, init_testK=0),
                 load_dir="../../Data/output_1.pkl",
                 filterout_hardcases=True,
-                savename="processed_120c_xyz.pkl",
-                proc_conf=ProcessingGaitConfig(fillZ_empty=False, preprocessing_conf=PreprocessingConfig(critical_limit=120)))
+                savename="processed_120c.pkl",
+                proc_conf=ProcessingGaitConfig(preprocessing_conf=PreprocessingConfig(critical_limit=120)))
             )
         config = BaseConfig(
-            try_num=27,
-            try_name="gcn3l_m2_I_60_offset_30",
+            try_num=40,
+            try_name="gcn3l_non_temporal",
             device="cuda:0",
             eval_batch_size=1,
             save_log_in_file=True,
@@ -36,17 +36,14 @@ class Entrypoint(E):
         self._edge_index: torch.Tensor = None
 
     def _model_forwarding(self, data):
-        x = data[0]
+        x = data[0][..., [0, 1]] # Use X-Y features
+        node_invalid = data[3].flatten(1).to(x.device)
+        node_valid = ~node_invalid
 
         if self._edge_index is None:
             self._edge_index = self._get_edges(x.size(1))
         x = x.flatten(1, -2) # N, T*V, D
-        data = Batch.from_data_list([Data(x=x_, edge_index=self._edge_index) for x_ in x])
+        data = Batch.from_data_list([Data(x=x_, edge_index=self._edge_index, edge_weight=calc_edge_weight(self._edge_index, nv)) for (x_, nv) in zip(x, node_valid)])
         data = data.to(x.device)
-        out = self.model(x=data.x, edge_index=data.edge_index, batch=data.batch)
+        out = self.model(x=data.x, edge_index=data.edge_index, batch=data.batch, edge_weight=data.edge_weight)
         return out
-
-    def get_model(self) -> nn.Module:
-        num_classes = self.kfold._ulabels.size
-        model = GCN_3l_BN(model_level='graph', dim_node=3, dim_hidden=60, num_classes=num_classes)
-        return model

src/STPGait/models/gcn_semisupervised.py
@@ -1,6 +1,5 @@
 from typing import Optional
 
-from dig.xgraph.models import GCN_3l_BN
 import numpy as np
 import torch
 from torch import nn
@@ -8,8 +7,9 @@ from torch_geometric.data import Batch, Data
 from torch.nn import functional as F
 
 from ..context import Skeleton
+from ..models import GCN_3l_BN
 
-def _calc_edge_weight(edge_index: torch.Tensor, node_valid: Optional[torch.Tensor]=None) -> Optional[torch.Tensor]:
+def calc_edge_weight(edge_index: torch.Tensor, node_valid: Optional[torch.Tensor]=None) -> Optional[torch.Tensor]:
     if node_valid is None:
         return None
     
@@ -58,9 +58,9 @@ class GCNSemiSupervised(nn.Module):
 
         if torch.any(labeled):
             data1 = Batch.from_data_list([Data(x=x_.flatten(end_dim=-2), edge_index=edge_index, 
-                edge_weight=_calc_edge_weight(edge_index, ni)) for x_, ni in zip(x[labeled], ~niv[labeled])])
+                edge_weight=calc_edge_weight(edge_index, ni)) for x_, ni in zip(x[labeled], ~niv[labeled])])
             
-            o_sup = self.gcn_supervised(x=data1.x, edge_index=data1.edge_index)
+            o_sup = self.gcn_supervised(x=data1.x, edge_index=data1.edge_index, batch=data1.batch, edge_weight=data1.edge_weight)
             
             olog_sup = F.log_softmax(o_sup)
             idx = torch.arange(olog_sup.size(0))
@@ -73,13 +73,13 @@ class GCNSemiSupervised(nn.Module):
         # Unsupervised forwarding
         ## 1st: Forward first part of body; e.g. lower part
         data2 = Batch.from_data_list([Data(x=x_.flatten(end_dim=-2), edge_index=edge_index_p1, 
-            edge_weight=_calc_edge_weight(edge_index_p1, nv)) for x_, nv in zip(x1, ~niv1)])
-        o1_unsup = self.gcn_unsupervised_lower(x=data2.x, edge_index=data2.edge_index)
+            edge_weight=calc_edge_weight(edge_index_p1, nv)) for x_, nv in zip(x1, ~niv1)])
+        o1_unsup = self.gcn_unsupervised_lower(x=data2.x, edge_index=data2.edge_index, batch=data2.batch, edge_weight=data2.edge_weight)
 
         ## 2nd: Forward 2nd part of body; e.g. upper part
         data3 = Batch.from_data_list([Data(x=x_.flatten(end_dim=-2), edge_index=edge_index_p2, 
-            edge_weight=_calc_edge_weight(edge_index_p2, nv)) for x_, nv in zip(x2, ~niv2)])
-        o2_unsup = self.gcn_unsupervised_upper(x=data3.x, edge_index=data3.edge_index)
+            edge_weight=calc_edge_weight(edge_index_p2, nv)) for x_, nv in zip(x2, ~niv2)])
+        o2_unsup = self.gcn_unsupervised_upper(x=data3.x, edge_index=data3.edge_index, batch=data3.batch, edge_weight=data3.edge_weight)
 
 
         o1log_unsup = F.log_softmax(o1_unsup)

src/STPGait/models/gcn_transformer.py
@@ -1,11 +1,11 @@
 from typing import List, Optional
 
-from dig.xgraph.models import GCN_3l_BN
 import torch
 from torch import nn
 from torch_geometric.data import Batch, Data
 
 from .gcn_lstm_transformer import TransformerEncoderConf
+from .gcn_3l_bn import GCN_3l_BN
 
 class GCNTransformer(GCN_3l_BN):
     def __init__(self, model_level: str, dim_node: int, 
@@ -27,18 +27,14 @@ class GCNTransformer(GCN_3l_BN):
 
         self.V = num_nodes
 
-    def forward(self, data: List[Data]) -> torch.Tensor:
-        N = len(data)
-        L, _ = data[0].x.shape
+    def forward(self, x: torch.Tensor, edge_index: torch.Tensor, batch: torch.Tensor, edge_weight: torch.Tensor = None) -> torch.Tensor:
+        N = x.size(0)
+        L, _ = x.size(1)
         T = L // self.V
 
-        with torch.no_grad():
-            X: Batch = Batch.from_data_list(data)
-            x, edge_index, batch = X.x, X.edge_index, X.batch
-
-        post_conv = self.relu1(self.conv1(x, edge_index))
+        post_conv = self.relu1(self.conv1(x, edge_index, edge_weight))
         for conv, relu in zip(self.convs, self.relus):
-            post_conv = relu(conv(post_conv, edge_index))
+            post_conv = relu(conv(post_conv, edge_index, edge_weight))
         
         post_conv = post_conv.reshape(N, T, self.V, -1)
         assert post_conv.size(1) == T and post_conv.size(2) == self.V, "Shape mismatch."