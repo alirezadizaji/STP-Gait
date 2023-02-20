b8f5154261e7ec27dfd3a32d2ff3c0d8c16ddec8
AlirezaDizaji <alirezadizaji@yahoo.com>
2023-02-20 12:53:27 +0330

    Fix

---
+++ src/STPGait/entrypoints/classification/gcn/try68.py
@@ -0,0 +1,55 @@
+from typing import List, Tuple
+
+import numpy as np
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch_geometric.data import Batch, Data
+
+from ....config import BaseConfig, TrainingConfig
+from ....context import Skeleton
+from ....dataset.KFold import GraphSkeletonKFoldOperator, SkeletonKFoldConfig, KFoldConfig
+from ....data.read_gait_data import ProcessingGaitConfig
+from ....enums import Separation, Optim
+from ....models.gcn_cct import GCNCCT
+from ....preprocess.main import PreprocessingConfig
+from ...train import TrainEntrypoint
+from .try63 import Entrypoint as E
+
+IN = Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]
+OUT = Tuple[torch.Tensor, torch.Tensor]
+
+# try 68
+## Remove unsupervised-branch totally; use only supervised branch.
+class Entrypoint(E):
+    def __init__(self) -> None:
+        kfold = GraphSkeletonKFoldOperator(
+            config=SkeletonKFoldConfig(
+                kfold_config=KFoldConfig(K=5, init_valK=0, init_testK=0, filterout_unlabeled=True),
+                load_dir="../../Data/output_1.pkl",
+                filterout_hardcases=True,
+                savename="processed_120c.pkl",
+                proc_conf=ProcessingGaitConfig(preprocessing_conf=PreprocessingConfig(critical_limit=120)))
+            )
+        config = BaseConfig(
+            try_num=68,
+            try_name="gcn_cct_sup_part",
+            device="cuda:0",
+            eval_batch_size=32,
+            save_log_in_file=True,
+            training_config=TrainingConfig(num_epochs=200, optim_type=Optim.ADAM, lr=3e-3, early_stop=50)
+        )
+        TrainEntrypoint.__init__(self, kfold, config)
+
+        self._edge_index: torch.Tensor = None
+
+    def _calc_loss(self, x: OUT, data: IN) -> torch.Tensor:
+        _, y, _, labeled = data
+        o_main, _ = x
+        
+        oml = o_main[labeled]
+        yl = y[labeled]
+        loss_sup = -torch.mean(oml[torch.arange(yl.numel()), yl])
+
+        loss = loss_sup
+        return loss
