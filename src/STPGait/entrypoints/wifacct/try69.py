8766d7308359b9912c8261582d56365063a3b2f8
AlirezaDizaji <alirezadizaji@yahoo.com>
2023-02-23 14:56:27 +0330

    Fix

--- src/STPGait/entrypoints/wifacct/try69.py
+++ src/STPGait/entrypoints/wifacct/try69.py
@@ -34,7 +34,7 @@ class Entrypoint(E):
             try_name="wifacct_msg3d",
             device="cuda:0",
             eval_batch_size=12,
-            save_log_in_file=False,
+            save_log_in_file=True,
             training_config=TrainingConfig(num_epochs=200, optim_type=Optim.ADAM, lr=3e-3, early_stop=50, batch_size=12)
         )
         TrainEntrypoint.__init__(self, kfold, config)
