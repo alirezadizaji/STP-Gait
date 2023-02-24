f1a39961655fbf4f730a0c3e330a1d89f554874c
Hedieh <hediehpourghasem@gmail.com>
2023-02-24 15:23:40 +0330

    Feature: implement WiFaCCT ST-GCN entrypoints

---
+++ src/STPGait/entrypoints/wifacct/try74.py
@@ -0,0 +1,50 @@
+from typing import List, Tuple
+
+import torch
+
+from ...config import BaseConfig, TrainingConfig
+from ...dataset.KFold import GraphSkeletonKFoldOperator, SkeletonKFoldConfig, KFoldConfig
+from ...data.read_gait_data import ProcessingGaitConfig
+from ...enums import Optim
+from ...preprocess.main import PreprocessingConfig
+from ..train import TrainEntrypoint
+from .try73 import Entrypoint as E
+
+IN = Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]
+OUT = Tuple[torch.Tensor, torch.Tensor]
+
+# try 74
+## Remove unsupervised-branch totally; use only supervised branch.
+class Entrypoint(E):
+    def __init__(self) -> None:
+        kfold = GraphSkeletonKFoldOperator(
+            config=SkeletonKFoldConfig(
+                kfold_config=KFoldConfig(K=5, init_valK=0, init_testK=0, filterout_unlabeled=True),
+                load_dir="../../Data/output_1.pkl",
+                filterout_hardcases=True,
+                savename="processed_120c.pkl",
+                proc_conf=ProcessingGaitConfig(preprocessing_conf=PreprocessingConfig(critical_limit=120)))
+            )
+        config = BaseConfig(
+            try_num=74,
+            try_name="wifacct_stgcn_sup_part",
+            device="cuda:0",
+            eval_batch_size=32,
+            save_log_in_file=True,
+            training_config=TrainingConfig(num_epochs=200, optim_type=Optim.ADAM, lr=3e-3, early_stop=50)
+        )
+        TrainEntrypoint.__init__(self, kfold, config)
+
+        self._edge_index: torch.Tensor = None
+
+    def _calc_loss(self, x: OUT, data: IN) -> torch.Tensor:
+        _, y, _, labeled = data
+        o_main, _ = x
+        m = torch.nn.LogSoftmax(dim=1)
+        o_main = m(o_main)
+        oml = o_main[labeled]
+        yl = y[labeled]
+        loss_sup = -torch.mean(oml[torch.arange(yl.numel()), yl])
+
+        loss = loss_sup
+        return loss
