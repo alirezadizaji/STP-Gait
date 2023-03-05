9385d48869342956dd54ad3a94b6fa364d8212d9
Hedieh <hediehpourghasem@gmail.com>
2023-03-05 14:24:38 +0330

    Fix SVM

--- src/STPGait/entrypoints/metaclass/try115.py
+++ src/STPGait/entrypoints/metaclass/try115.py
@@ -3,11 +3,10 @@ from typing import List, Tuple
 import numpy as np
 import torch
 from torch import nn
-from torch.nn import functional as F
-from torch_geometric.data import Batch, Data
 
+from scipy.stats import chi2_contingency
 from sklearn import svm
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import multilabel_confusion_matrix, f1_score, roc_auc_score, accuracy_score, precision_score, recall_score
 
 from ...config import BaseConfig, TrainingConfig
 from ...context import Skeleton
@@ -40,7 +39,7 @@ class Entrypoint(TrainEntrypoint[IN, OUT, BaseConfig]):
             try_name="wifacct_svm_balanced_s",
             device="cuda:0",
             eval_batch_size=12,
-            save_log_in_file=False,
+            save_log_in_file=True,
             training_config=TrainingConfig(num_epochs=200, optim_type=Optim.ADAM, lr=3e-3, early_stop=50, batch_size=12)
         )
         TrainEntrypoint.__init__(self, kfold, config)
@@ -57,19 +56,42 @@ class Entrypoint(TrainEntrypoint[IN, OUT, BaseConfig]):
         self.model.fit(X_train, Y_train)
 
         X_test = self.val_loader.dataset.X.flatten(1)
-        Y_test = self.val_loader.dataset.Y
-        Y_pred = self.model.predict(X_test)
+        self.y_gt = self.val_loader.dataset.Y
+        self.y_pred = self.model.predict(X_test)
 
-        self.evaluation(Y_test, Y_pred)
+        self.evaluation()
 
-    def evaluation(self, y_true, y_pred):
-        # Accuracy
-        accuracy = accuracy_score(y_true, y_pred)
-        # F1 score
-        f1score = f1_score(y_true, y_pred, average='macro') # macro: Calculate metrics for each label, and find their unweighted mean. This does not take label imbalance into account.
+    def evaluation(self):
+        num_classes = self.kfold._ulabels.size
+        num_samples = len(self.y_pred)
 
-        print(f'Mean accuracy score: {accuracy:.3}')
-        print(f'Mean F1 score (macro): {f1score:.3}')
+        mcm = multilabel_confusion_matrix(self.y_gt, self.y_pred)
+        tps, tns = mcm[:, 1, 1], mcm[:, 0, 0]
+        fns, fps = mcm[:, 1, 0], mcm[:, 0, 1]
+
+        acc = accuracy_score(self.y_gt, self.y_pred) * 100
+        spec = np.mean((tns) / (tns + fps)) * 100
+        sens = np.mean((tps) / (tps + fns)) * 100
+        y_pred_one_hot = np.zeros((num_samples, num_classes), dtype=np.int64)
+        y_pred_one_hot[np.arange(num_samples), self.y_pred] = 1
+        
+        f1 = f1_score(self.y_gt, self.y_pred, average='macro')
+        auc = roc_auc_score(self.y_gt, y_pred_one_hot, multi_class='ovr') * 100
+        rec = recall_score(self.y_gt, self.y_pred, average='macro') * 100
+        pre = precision_score(self.y_gt, self.y_pred, average='macro') * 100
+
+        observed = np.zeros((2, num_classes))
+        np.add.at(observed[0], self.y_pred, 1)
+        np.add.at(observed[1], self.y_gt, 1)
+        _, p, *_ = chi2_contingency(observed)
+
+        print(f'acc {acc:.2f} spec {spec:.2f} sens {sens:.2f} f1 {f1:.2f} auc {auc:.2f} p-value {p:.3f} precision {pre:.2f} recall {rec:.2f}.', flush=True)
+            
+        return np.array([acc, f1, sens, spec, auc, rec, pre, p])
+
+    @property
+    def criteria_names(self) -> List[str]:
+        return super().criteria_names + ['ACC', 'F1', 'Sens', 'Spec', 'AUC', 'Recall', 'Precision', 'P']
 
     def _calc_loss(self, x, data):
         pass
