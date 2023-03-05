e8a6675962460ae4cc2d5e087920e660b4055ebd
AlirezaDizaji <alirezadizaji@yahoo.com>
2023-03-05 10:56:14 +0330

    Feature: move try111 to dynamic coefficient part

--- src/STPGait/entrypoints/wifacct/dynamic_coef/try111.py
+++ src/STPGait/entrypoints/wifacct/dynamic_coef/try111.py
@@ -2,13 +2,13 @@ from typing import List, Tuple
 
 import torch
 
-from ...config import BaseConfig, TrainingConfig
-from ...dataset.KFold import GraphSkeletonKFoldOperator, SkeletonKFoldConfig, KFoldConfig
-from ...data.read_gait_data import ProcessingGaitConfig
-from ...enums import Optim
-from ...preprocess.main import PreprocessingConfig
-from ..train import TrainEntrypoint
-from .try75 import Entrypoint as E
+from ....config import BaseConfig, TrainingConfig
+from ....dataset.KFold import GraphSkeletonKFoldOperator, SkeletonKFoldConfig, KFoldConfig
+from ....data.read_gait_data import ProcessingGaitConfig
+from ....enums import Optim
+from ....preprocess.main import PreprocessingConfig
+from ...train import TrainEntrypoint
+from ..try75 import Entrypoint as E
 
 IN = Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]
 OUT = Tuple[torch.Tensor, torch.Tensor]
@@ -41,3 +41,23 @@ class Entrypoint(E):
     @property
     def frame_size(self):
         return 360
+    
+    def _calc_loss(self, x, data):
+        _, y, _, labeled = data
+        o_main, o_aux = x
+        
+        oml = o_main[labeled]
+        yl = y[labeled]
+        loss_sup = -torch.mean(oml[torch.arange(yl.numel()), yl])
+
+        y1d = o_main.argmax(1).detach().unsqueeze(1).repeat(1, o_aux.size(1)).flatten()
+        o_aux = o_aux.flatten(0, 1)
+        loss_unsup = -torch.mean(o_aux[torch.arange(y1d.size(0)), y1d])
+
+        u = int(self.epoch >= self._start_ul_epoch or not self.model.training)
+        if not torch.isnan(loss_sup):
+            alpha = loss_sup.detach() / loss_unsup.detach()
+            loss = loss_sup + u * alpha * loss_unsup
+        else:
+            loss = loss_unsup
+        return loss
