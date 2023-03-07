8a4981cb266f9c0bbed3d4fec5fe68cb119b7267
AlirezaDizaji <alirezadizaji@yahoo.com>
2023-03-07 19:32:12 +0330

    Fix: try 104

--- src/STPGait/entrypoints/conditions/try104.py
+++ src/STPGait/entrypoints/conditions/try104.py
@@ -2,12 +2,15 @@ from typing import List, Tuple
 
 import numpy as np
 import torch
+from torch import nn
 
 from ...config import BaseConfig, TrainingConfig
 from ...dataset.KFold import GraphSkeletonKFoldOperator, SkeletonKFoldConfig, KFoldConfig
 from ...data.read_gait_data import ProcessingGaitConfig
 from ...enums import Separation, Optim, Label
 from ...preprocess.main import PreprocessingConfig
+from ...models.wifacct import WiFaCCT
+from ...models.wifacct.mst_gcn import Model1, Model2
 from ..train import TrainEntrypoint
 from ..wifacct.try78 import Entrypoint as E
 
@@ -37,9 +40,69 @@ class Entrypoint(E):
             try_name="cond3class_MSTGCN_sup_" + self.cond,
             device="cuda:0",
             eval_batch_size=32,
-            save_log_in_file=False,
+            save_log_in_file=True,
             training_config=TrainingConfig(num_epochs=200, optim_type=Optim.ADAM, lr=3e-3, early_stop=50)
         )
         TrainEntrypoint.__init__(self, kfold, config)
 
         self._edge_index: torch.Tensor = None
+
+    def get_model(self) -> nn.Module:
+        
+        in_channels = 2
+        num_point = 25
+        num_person=1
+        num_classes = self.kfold._ulabels.size
+        basic_channels = 60
+
+        cfgs1 = {
+        'num_class': num_classes,
+        'num_point': 25,
+        'num_person': 1,
+        'block_args': [[2, basic_channels, False, 1],
+                       [basic_channels, basic_channels, True, 1]],
+        # 'graph': 'graph.kinetics.Graph',
+        'graph_args': {'labeling_mode': 'spatial'},
+        'kernel_size': 9,
+        'block_type': 'ms',
+        'reduct_ratio': 2,
+        'expand_ratio': 0,
+        't_scale': 4,
+        'layer_type': 'sep',
+        'act': 'relu',
+        's_scale': 4,
+        'atten': 'stcja',
+        'bias': True,
+        # 'parts': parts
+        }
+        model1 = Model1(**cfgs1)
+
+        cfgs2 = {
+        'A': model1.A,
+        'num_class': num_classes,
+        'num_point': 25,
+        'num_person': 1,
+        'block_args': [[basic_channels, basic_channels, True, 1]],
+        # 'graph': 'graph.kinetics.Graph',
+        'graph_args': {'labeling_mode': 'spatial'},
+        'kernel_size': 9,
+        'block_type': 'ms',
+        'reduct_ratio': 2,
+        'expand_ratio': 0,
+        't_scale': 4,
+        'layer_type': 'sep',
+        'act': 'relu',
+        's_scale': 4,
+        'atten': 'stcja',
+        'bias': True,
+        # 'parts': parts
+        }
+        model2 = Model2(**cfgs2)
+        if self.cond == 'DTM':
+            num_frames = 317
+        elif self.cond == 'EC':
+            num_frames = 464
+        elif self.cond == 'PS':
+            num_frames = 366
+        model = WiFaCCT[Model1, Model2](model1, model2, num_frames=num_frames, num_aux_branches=3)
+        return model
