135392f2925c51efe0a60d874abe62cccd4aac6e
AlirezaDizaji <alirezadizaji@yahoo.com>
2023-03-07 20:13:32 +0330

    Fix: try 118

--- src/STPGait/entrypoints/wifacct/dynamic_coef/try118.py
+++ src/STPGait/entrypoints/wifacct/dynamic_coef/try118.py
@@ -44,7 +44,7 @@ class Entrypoint(E):
         self._edge_index: torch.Tensor = None
         self._start_ul_epoch: int = 10
 
-        self.rerun: bool = False
+        self.org_model_res = "108_wifacct_gcn3l_balanced_s"
     
     def set_loaders(self) -> None:
         # DO NOT SHUFFLE
@@ -77,48 +77,39 @@ class Entrypoint(E):
         loader.dataset.Y[~loader.dataset.labeled] = y_pred[~loader.dataset.labeled]
 
     def _calc_loss(self, x: OUT, data: IN) -> torch.Tensor:
-        _, y, _, labeled = data
+        y = data[1]
         o_main, _ = x
-        if self.rerun:
-            labeled = torch.ones_like(y, dtype=np.bool)
 
-        oml = o_main[labeled]
-        yl = y[labeled]
-        loss_sup = -torch.mean(oml[torch.arange(yl.numel()), yl])
+        loss_sup = -torch.mean(o_main[torch.arange(y.numel()), y])
 
         return loss_sup
 
     def run(self):
         self.fold_test_criterion: np.ndarray = np.full((len(self.criteria_names), self.kfold.K), fill_value=-np.inf)
-        self.fold_test_criterion_rerun = np.full_like(self.fold_test_criterion, fill_value=-np.inf)
         print(f"@@@@@@@@@@@@ PHASE {self.conf.phase} IN PROGRESS... @@@@@@@@@@@@", flush=True)
         
         for self.current_K in range(self.kfold.K):
             with self.kfold:
-                self._sub_run()
-            
-                files = os.listdir(self.weight_save_dir)
+                weight_save_dir = os.path.join("../results", self.org_model_res, "weights", f"{self.kfold.valK}-{self.kfold.testK}")
+
+                files = os.listdir(weight_save_dir)
                 if len(files) != 1:
-                    print(f"@@@ WARNING: {self.weight_save_dir} directory should have exactly one saved file; got {len(files)} instead.", flush=True)
+                    print(f"@@@ WARNING: {weight_save_dir} directory should have exactly one saved file; got {len(files)} instead.", flush=True)
                 
-                self.epoch = int(files[0])
-                self.model.load_state_dict(torch.load(self._get_weight_save_path(self.epoch), map_location=self.conf.device))
+                epoch = int(files[0])
+                weight_save_path =  os.path.join(weight_save_dir, str(epoch))
+                self.model.load_state_dict(torch.load(weight_save_path, map_location=self.conf.device))
 
                 # labeling
                 with torch.no_grad():
                     self.model.eval()
                     for loader in [self.train_loader, self.val_loader, self.test_loader]:
                         self._relabel(loader)
-                    self.rerun = True
                 
                 self._sub_run()
-                self.rerun = False
 
         cv = {c: np.around(np.mean(v), decimals=4) for c, v in zip(self.criteria_names, self.fold_test_criterion)}
         print(f"@@@ Final Result on {self.kfold.K} KFold operation on test set: {cv} @@@", flush=True)
-
-        cv = {c: np.around(np.mean(v), decimals=4) for c, v in zip(self.criteria_names, self.fold_test_criterion_rerun)}
-        print(f"@@@ Final Result on rerun {self.kfold.K} KFold operation on test set: {cv} @@@", flush=True)
         
     def _main_phase_train(self):
         num_epochs = self.conf.training_config.num_epochs
@@ -155,10 +146,7 @@ class Entrypoint(E):
         
         criteria = self._validate_for_one_epoch(Separation.TEST, self.test_loader)
         test = criteria[self.best_epoch_criterion_idx]
-        if self.rerun:
-            self.fold_test_criterion_rerun[:, self.kfold.testK] = criteria
-        else:
-            self.fold_test_criterion[:, self.kfold.testK] = criteria
+        self.fold_test_criterion[:, self.kfold.testK] = criteria
         
         val = self._criteria_vals[self._VAL_CRITERION_IDX, self.epoch, self.best_epoch_criterion_idx]
         
@@ -177,10 +165,7 @@ class Entrypoint(E):
         val = self._criteria_vals[self._VAL_CRITERION_IDX, self.epoch, self.best_epoch_criterion_idx]
         
         criteria = self._validate_for_one_epoch(Separation.TEST, self.test_loader)
-        if self.rerun:
-            self.fold_test_criterion_rerun[:, self.kfold.testK] = criteria
-        else:
-            self.fold_test_criterion[:, self.kfold.testK] = criteria
+        self.fold_test_criterion[:, self.kfold.testK] = criteria
         test = criteria[self.best_epoch_criterion_idx]
         
         print(f"@@@@@ Best criteria ValK {self.kfold.valK} epoch {self.epoch} @@@@@\n---> criterion: {self.criteria_names[self.best_epoch_criterion_idx]}, val: {val:.2f}, test: {test:.2f} <----", flush=True)
